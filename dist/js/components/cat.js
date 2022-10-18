96fa6c7ef064c03fed8fd0082cd427bcd21716ca
Liluminalia <peloazul4@gmail.com>
2022-10-19 00:19:50 +0200

    adding cat wrong

--- dist/js/components/cat.js
+++ dist/js/components/cat.js
@@ -1,1 +1,21 @@
-"use strict";
+export class Cat {
+    constructor(name, kind, race, parents) {
+        this.name = name;
+        this.kind = kind;
+        this.race = race;
+        this.parents = parents;
+        this.directory = './cat.html';
+    }
+    createTemplate() {
+        let template = `
+        
+            <section>
+                <h2>NERON</h2>
+                 <img src="./dist/img/cat.jpg" alt="foto de un gato gris">
+                
+            </section>
+        
+        `;
+        return template;
+    }
+}
---
+++ src/components/cat.ts
@@ -0,0 +1,29 @@
+import { AddPet } from './add.pet.js';
+import { Footer } from './footer.js';
+import { Header } from './header.js';
+import { Main } from './main.js';
+import { PetList } from './pet.list.js';
+
+export class Cat {
+    directory: string;
+    constructor(
+        public name: string,
+        public kind: string,
+        public race: string,
+        public parents: string
+    ) {
+        this.directory = './cat.html';
+    }
+    createTemplate() {
+        let template = `
+        
+            <section>
+                <h2>NERON</h2>
+                 <img src="./dist/img/cat.jpg" alt="foto de un gato gris">
+                
+            </section>
+        
+        `;
+        return template;
+    }
+}
