1a83b7b887c7094759ddca537efafb7c2ea7240b
Liluminalia <peloazul4@gmail.com>
2022-10-18 23:07:11 +0200

    adding index js

---
+++ dist/js/index.js
@@ -0,0 +1,8 @@
+import { Footer } from './components/footer.js';
+import { Header } from './components/header.js';
+import { Main } from './components/main.js';
+import { PetList } from './components/pet.list.js';
+new Header('body');
+new Main('body');
+new PetList('main');
+new Footer('body');
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { AddPet } from './components/add.pet.js';
+import { Footer } from './components/footer.js';
+import { Header } from './components/header.js';
+import { Main } from './components/main.js';
+import { PetList } from './components/pet.list.js';
+
+new Header('body');
+new Main('body');
+new PetList('main');
+new Footer('body');
